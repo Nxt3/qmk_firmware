8df2bdd2ca30d1338b4e13c9ce73d0a73443f454
Nxt3 <nategantt@gmail.com>
2019-08-05 12:41:34 -0400

    Added the ZELDA_PUZZLE song back to my config.h

--- keyboards/clueboard/66_hotswap/keymaps/nxt3/config.h
+++ keyboards/clueboard/66_hotswap/keymaps/nxt3/config.h
@@ -1,6 +1,16 @@
 // NXT3 custom overrides for various configs
 #pragma once
 
+#define ZELDA_PUZZLE_ALT \
+    Q__NOTE(_G5),     \
+    Q__NOTE(_FS5),    \
+    Q__NOTE(_DS5),     \
+    Q__NOTE(_A4),    \
+    Q__NOTE(_GS4),     \
+    Q__NOTE(_E5),     \
+    Q__NOTE(_GS5),     \
+    HD_NOTE(_C6),
+
 #ifdef AUDIO_ENABLE
   #undef DAC_SAMPLE_MAX
   #undef STARTUP_SONG
@@ -8,5 +18,5 @@
 
   #define DAC_SAMPLE_MAX 2000U
   #define STARTUP_SONG SONG(TERMINAL_SOUND)
-  #define GOODBYE_SONG SONG(ZELDA_PUZZLE)
+  #define GOODBYE_SONG SONG(ZELDA_PUZZLE_ALT)
 #endif
